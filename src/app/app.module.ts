f0343120cfdd172eb2f68549057ce5c521209c66
Bikmay <artyom.ivliev@gmail.com>
2019-03-21 13:18:33 +0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {FormsModule} from '@angular/forms';
+import { MatMenuModule} from '@angular/material/menu';
+import { MainComponent } from './main/main.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {RouterModule} from "@angular/router";
+import { MaterialModule } from '../material.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+
+
+const routes =[
+  {path:'', component: MainComponent},
+  {path:'registration', component: RegistrationComponent}
+]
+
+@NgModule({
+  declarations: [
+
+    AppComponent,
+    MainComponent,
+    RegistrationComponent,
+
+
+  ],
+  imports: [
+
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatMenuModule,
+    RouterModule.forRoot(routes),
+    MaterialModule,
+    MatSidenavModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.less']
+})
+export class MainComponent implements OnInit {
+
+  events: string[] = [];
+  opened: boolean;
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
